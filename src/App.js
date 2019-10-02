1d045a44bfe00c56d68bba9346e6e9518439b226
Jaken Herman <JakenHerman7@Gmail.com>
2019-10-01 20:36:43 -0500

    created base project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,69 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as axios from 'axios';
+import {Grid, Segment, Header} from 'semantic-ui-react';
+import { AgGridReact } from 'ag-grid-react';
+import 'ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-balham.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello Heroku!</h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor (props) {
+    super(props);
+    this.state = {
+      columnDefs: [{
+        headerName: "Name", field: "name"
+      }, {
+        headerName: "Best Sell Price", field: "best_sell_price"
+      }, {
+        headerName: "Best Buy Price", field: "best_buy_price"
+      }],
+      rowData: []
+    }
+  }
+
+  getListings() {
+    axios.get('https://cors-anywhere.herokuapp.com/https://mlb19.theshownation.com/apis/listings.json')
+    .then((response) => {
+      this.setState({
+        rowData: response.data
+      })
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+  }
+
+  componentDidMount () {
+    this.getListings();
+  }
+
+  render() {
+    return (
+        <Grid columns={3}>
+          <Grid.Row>
+            <Grid.Column width={3}></Grid.Column>
+            <Grid.Column width={10}>
+              <br />
+              <Header>MLB The Show 19 Community Market Listings</Header>
+              <Segment>
+                <div 
+                  className="ag-theme-balham"
+                  style={{ 
+                  height: '500px', 
+                  width: '920px' }} 
+                >
+                  <AgGridReact
+                    columnDefs={this.state.columnDefs}
+                    rowData={this.state.rowData.listings}>
+                  </AgGridReact>
+                </div>
+              </Segment>
+            </Grid.Column>
+            <Grid.Column width={3}></Grid.Column>
+          </Grid.Row>
+        </Grid>
+    );
+  }
 }
 
 export default App;
