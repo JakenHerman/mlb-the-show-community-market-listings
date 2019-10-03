1c2f17e7336451cde28cabda37c5a6ddd3152d20
tom-raley <tom.raley@gmail.com>
2019-10-02 21:42:00 -0500

    added eslint

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
+/* eslint-disable */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import 'semantic-ui-css/semantic.min.css'
+/* eslint enable */
 
 ReactDOM.render(<App />, document.getElementById('root'));
